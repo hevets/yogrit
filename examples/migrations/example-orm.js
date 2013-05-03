5a10c1a226d92bb69c7624036f5043ecba2a7be3
Steve Henderson <hevets@gmail.com>
2013-05-02 22:01:40 -0700

    Committing only the good stuff

--- examples/migrations/example-orm.js
+++ examples/migrations/example-orm.js
@@ -1,25 +1,29 @@
-var People = require('');
 
 module.exports = {
   
   query: function(next) {
-
+    console.log('query')
+    next();
   },
 
   pre: function(next) {
-
+    console.log('pre')
+    next();
   },
 
   mutate: function(next) {
-
+    console.log('mutate')
+    next();
   },
 
   post: function(next) {
-
+    console.log('post')
+    next();
   },
 
   save: function(next) {
-    
+    console.log('save')
+    next();
   }
 
 };
